9ff9455d93c2c80b26205862a8edfe4b86e3b699
Ana Melnik <79505183+AnaMelnik@users.noreply.github.com>
2021-03-04 10:20:04 +0200

    Update 10.b2_skolenu_darbi.py
    Rindā Nr.2 pievienoju matemātisku darbību cos60+0.5

--- 10.b2_skolenu_darbi.py
+++ 10.b2_skolenu_darbi.py
@@ -1,26 +1,26 @@
 1 	
 Kudrjavceva Veronika
-2 	
+2 	print(cos(radians(60)+fmod(15,2)))
 Melnic Ana
 3 	
 Mihailovs Alekss
 4 	
 Orlova Katerina
 5 	
-Rimare Sabîne
+Rimare SabÃ®ne
 6 	
 Sidorenko Aleksandrs
 7 	
-Siïiòğ Deniss
+SiÃ¯iÃ²Ã° Deniss
 8 	
 Skodorovs Vlads
 9 	
 Smirnovs Volodars
 10 	
-Verşanovskis Marks
+VerÃ¾anovskis Marks
 11 	
 Volkova Veronika
 12 	
-Zariòğ Roberts
+ZariÃ²Ã° Roberts
 13 	
-Şaravins Kirills
+Ãaravins Kirills
